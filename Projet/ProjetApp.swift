3ea6bb45e34549bd49eb1e4545a0fd9d70d7fcc0
ericprojet <eric.wu@efrei.net>
2022-02-14 13:20:21 +0100

    Initial Commit

---
+++ Projet/ProjetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetApp.swift
+//  Projet
+//
+//  Created by goldorak on 14/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct ProjetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
